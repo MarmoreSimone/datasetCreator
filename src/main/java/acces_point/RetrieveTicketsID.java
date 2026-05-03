1b854adaf3b9a06857f189d36304d4157ff35160
simone <test@gmail.com>
2026-05-03 18:43:25 +0200

    sonarCloud: cambiato RetrieveTicketsID

--- src/main/java/acces_point/RetrieveTicketsID.java
+++ src/main/java/acces_point/RetrieveTicketsID.java
@@ -1,5 +1,4 @@
 package acces_point;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -11,6 +10,74 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RetrieveTicketsID {
+
+    private static final String PROJ_NAME = "OPENJPA";
+    private static final int PAGE_SIZE = 1000;
+
+    static void main() throws IOException, JSONException {
+        String outputFile = "jiraTicketsEnriched.csv";
+        System.out.printf("Inizio download ticket Jira per il progetto %s...%n", PROJ_NAME);
+
+        try (PrintWriter writer = new PrintWriter(new FileWriter(outputFile))) {
+            writer.println("TicketID,CreationDate,ResolutionDate,AffectedVersions");
+            fetchAndWriteIssues(writer);
+            System.out.printf("%nCompletato! File salvato in: %s%n", outputFile);
+        }
+    }
+
+    private static void fetchAndWriteIssues(PrintWriter writer) throws IOException, JSONException {
+        int i = 0;
+        int total;
+        do {
+            int maxResults = i + PAGE_SIZE;
+            String url = String.format(
+                    "https://issues.apache.org/jira/rest/api/2/search?jql=project=%%22%s%%22AND%%22issueType%%22=%%22Bug%%22AND(%%22status%%22=%%22closed%%22OR%%22status%%22=%%22resolved%%22)AND%%22resolution%%22=%%22fixed%%22&fields=key,resolutiondate,versions,created&startAt=%d&maxResults=%d",
+                    PROJ_NAME, i, maxResults);
+
+            JSONObject json = readJsonFromUrl(url); // Assumi che questo metodo esista come prima
+            JSONArray issues = json.getJSONArray("issues");
+            total = json.getInt("total");
+
+            for (int k = 0; k < issues.length() && i < total; k++, i++) {
+                processAndWriteIssue(writer, issues.getJSONObject(k));
+            }
+
+            System.out.printf("Scaricati %d ticket su %d...%n", i, total);
+        } while (i < total);
+    }
+
+    private static void processAndWriteIssue(PrintWriter writer, JSONObject issue) throws JSONException {
+        String key = issue.getString("key");
+        JSONObject fields = issue.getJSONObject("fields");
+
+        String createdDate = fields.isNull("created") ? "NONE"
+                : fields.getString("created").substring(0, 10);
+
+        String resolvedDate = fields.isNull("resolutiondate") ? "NONE"
+                : fields.getString("resolutiondate").substring(0, 10);
+
+        String versionsStr = getVersionsString(fields);
+
+        writer.printf("%s,%s,%s,%s%n", key, createdDate, resolvedDate, versionsStr);
+    }
+
+    private static String getVersionsString(JSONObject fields) throws JSONException {
+        if (fields.isNull("versions")) {
+            return "NONE";
+        }
+
+        JSONArray versionsArray = fields.getJSONArray("versions");
+        if (versionsArray.length() == 0) {
+            return "NONE";
+        }
+
+        List<String> versionNames = new ArrayList<>();
+        for (int k = 0; k < versionsArray.length(); k++) {
+            versionNames.add(versionsArray.getJSONObject(k).getString("name"));
+        }
+
+        return String.join(";", versionNames);
+    }
 
     private static String readAll(Reader rd) throws IOException {
         StringBuilder sb = new StringBuilder();
@@ -26,77 +93,6 @@ public class RetrieveTicketsID {
              BufferedReader rd = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8))) {
             String jsonText = readAll(rd);
             return new JSONObject(jsonText);
-        }
-    }
-
-    static void main() throws IOException, JSONException {
-
-        String projName = "OPENJPA";
-        Integer j = 0;
-        Integer i = 0;
-        Integer total = 1;
-        String outputFile = "jiraTicketsEnriched.csv";
-
-        System.out.println("Inizio download ticket Jira per il progetto " + projName + "...");
-
-        try (PrintWriter writer = new PrintWriter(new FileWriter(outputFile))) {
-            // Scriviamo l'intestazione del CSV
-            writer.println("TicketID,CreationDate,ResolutionDate,AffectedVersions");
-
-            do {
-                // Pagina di 1000 risultati alla volta
-                j = i + 1000;
-                String url = "https://issues.apache.org/jira/rest/api/2/search?jql=project=%22"
-                        + projName + "%22AND%22issueType%22=%22Bug%22AND(%22status%22=%22closed%22OR"
-                        + "%22status%22=%22resolved%22)AND%22resolution%22=%22fixed%22&fields=key,resolutiondate,versions,created&startAt="
-                        + i.toString() + "&maxResults=" + j.toString();
-
-                JSONObject json = readJsonFromUrl(url);
-                JSONArray issues = json.getJSONArray("issues");
-                total = json.getInt("total");
-
-                for (; i < total && i < j; i++) {
-                    // Prendi l'oggetto ticket corrente
-                    JSONObject issue = issues.getJSONObject(i % 1000);
-                    String key = issue.getString("key");
-
-                    JSONObject fields = issue.getJSONObject("fields");
-
-                    // 1. Data Creazione (OV)
-                    String createdDate = "NONE";
-                    if (!fields.isNull("created")) {
-                        createdDate = fields.getString("created").substring(0, 10);
-                    }
-
-                    // 2. Data Risoluzione (FV)
-                    String resolvedDate = "NONE";
-                    if (!fields.isNull("resolutiondate")) {
-                        resolvedDate = fields.getString("resolutiondate").substring(0, 10);
-                    }
-
-                    // 3. Affected Versions (IV)
-                    String versionsStr = "NONE";
-                    if (!fields.isNull("versions")) {
-                        JSONArray versionsArray = fields.getJSONArray("versions");
-                        List<String> versionNames = new ArrayList<>();
-                        for (int k = 0; k < versionsArray.length(); k++) {
-                            versionNames.add(versionsArray.getJSONObject(k).getString("name"));
-                        }
-                        if (!versionNames.isEmpty()) {
-                            // Usiamo ';' per evitare problemi col CSV
-                            versionsStr = String.join(";", versionNames);
-                        }
-                    }
-
-                    // Scrivi la riga nel file CSV
-                    writer.printf("%s,%s,%s,%s%n", key, createdDate, resolvedDate, versionsStr);
-                }
-
-                System.out.println("Scaricati " + i + " ticket su " + total + "...");
-
-            } while (i < total);
-
-            System.out.println("\nCompletato! File salvato in: " + outputFile);
         }
     }
 }
